55b5f2808c97bf085cc5a0cc81a7697111323ae4
EvilSpirit <anandamide@mail.ru>
2020-02-16 17:13:30 +0700

    add SharpFont

---
+++ Assets/Code/Utils/Encrypton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public static class Encrypton {
+	public static string Xor(string text, uint key) {
+		StringBuilder builder = new StringBuilder(text.Length);  
+		for (int i = 0; i < text.Length; i++) {  
+			builder.Append((char)(text[i] ^ key));
+		}  
+		return builder.ToString();  
+	} 
+}
