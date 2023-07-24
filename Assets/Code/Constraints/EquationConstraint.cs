c94f42610e7632d28be9f24b471b1a7cb31d2913
Alexey Egorov <evilspirit@evilspirit.org>
2023-07-24 10:12:52 +0700

    fix equation constraint lost ability to edit after serialization

--- Assets/Code/Constraints/EquationConstraint.cs
+++ Assets/Code/Constraints/EquationConstraint.cs
@@ -9,7 +9,9 @@ public class EquationConstraint : ValueConstraint {
 	public EquationConstraint(Sketch sk) : base(sk) {
 		expression.isEquation = true;
 	}
-	public EquationConstraint(Sketch sk, Id id) : base(sk, id) { }
+	public EquationConstraint(Sketch sk, Id id) : base(sk, id) {
+		expression.isEquation = true;
+	}
 
 	public override ValueUnits units => ValueUnits.ARBITRARY;
 }
